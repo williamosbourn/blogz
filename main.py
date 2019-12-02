4b35ee095ce79c6406e460c40261bbe3da161dca
William Osbourn <william@osbourn.us>
2019-12-01 16:48:17 -0800

    set up templates and database

---
+++ main.py
@@ -0,0 +1,30 @@
+from flask import Flask, request, redirect, render_template, session, flash
+from flask_sqlalchemy import SQLAlchemy 
+
+app = Flask(__name__)
+app.config['DEBUG'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:password@localhost:8889/build-a-blog'
+app.config['SQLALCHEMY_ECHO'] = True
+db = SQLAlchemy(app)
+app.secret_key = 'y337kGcys&zP3B'
+
+class Blog(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(120))
+    body = db.Column(db.Text)  
+
+    def __init__(self, title, body):
+        self.title = title
+        self.body = body
+        
+
+
+
+@app.route('/', methods=['POST', 'GET'])
+def index():
+    return render_template('blog.html',title="Build A Blog")
+
+
+
+if __name__ == '__main__':
+    app.run()
